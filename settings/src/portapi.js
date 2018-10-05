45bf2d60c2d7ee537b05f4a0bf133da0f0e950d4
Daniel <dhaavi@users.noreply.github.com>
2018-10-05 22:17:25 +0200

    Finish first version of portapi.js

--- settings/src/portapi.js
+++ settings/src/portapi.js
@@ -13,40 +13,111 @@ function install(vue, options) {
 
   // vue.prototype.$queries = {}
   vue.prototype.$api = {
-    // 127|qsub|<query>
-    //    127|ok|<key>|<data>
-    //    127|done
-    //    127|error|<message>
-    //    127|upd|<key>|<data>
-    //    127|new|<key>|<data>
-    //    127|delete|<key>|<data>
-    //    127|warning|<message> // error with single record, operation continues
+    get(key, name) {
+      // 123|get|<key>
+      //    123|ok|<key>|<data>
+      //    123|error|<message>
+      return vue.portapi.newRequest(name, "get", key);
+    },
+    query(query, name) {
+      // 124|query|<query>
+      //    124|ok|<key>|<data>
+      //    124|done
+      //    124|error|<message>
+      //    124|warning|<message> // error with single record, operation continues
+      return vue.portapi.newRequest(name, "query", query);
+    },
+    sub(query, name) {
+      // 125|sub|<query>
+      //    125|upd|<key>|<data>
+      //    125|new|<key>|<data>
+      //    125|delete|<key>|<data>
+      //    125|warning|<message> // error with single record, operation continues
+      return vue.portapi.newRequest(name, "sub", query);
+    },
     qsub(query, name) {
+      // 127|qsub|<query>
+      //    127|ok|<key>|<data>
+      //    127|done
+      //    127|error|<message>
+      //    127|upd|<key>|<data>
+      //    127|new|<key>|<data>
+      //    127|delete|<key>|<data>
+      //    127|warning|<message> // error with single record, operation continues
       return vue.portapi.newRequest(name, "qsub", query);
     },
+
+    create(key, data, name) {
+      // 128|create|<key>|<data>
+      //    128|success
+      //    128|error|<message>
+      return vue.portapi.newRequest(name, "create", key, data);
+    },
+    update(key, data, name) {
+      // 129|update|<key>|<data>
+      //    129|success
+      //    129|error|<message>
+      return vue.portapi.newRequest(name, "update", key, data);
+    },
+    insert(key, data, name) {
+      // 130|insert|<key>|<data>
+      //    130|success
+      //    130|error|<message>
+      return vue.portapi.newRequest(name, "insert", key, data);
+    },
+    delete(key, name) {
+      // 131|delete|<key>
+      //    131|success
+      //    131|error|<message>
+      return vue.portapi.newRequest(name, "delete", key);
+    }
   }
   vue.portapi = {
     debug: (options.debug == true) ? true : false,
     conn: null,
+    connected: false,
+    requestQueue: [],
     url: options.url,
     requests: {},
     requestCnt: 0,
     connect() {
-  		vue.portapi.conn = new WebSocket(vue.portapi.url)
+  		vue.portapi.ws = new WebSocket(vue.portapi.url)
 
-  		vue.portapi.conn.onopen = function() {
+  		vue.portapi.ws.onopen = function() {
   	    if (vue.portapi.debug) console.log('connection to api established');
+
+        vue.portapi.connected = true
+
+        // send queued requests
+        vue.portapi.requestQueue.forEach(function(requestText) {
+          vue.portapi.ws.send(requestText);
+          if (vue.portapi.debug) console.log('sending queued request: ' + requestText);
+        });
+        vue.portapi.requestQueue = []
   		}
 
-  		vue.portapi.conn.onerror = function (error) {
+  		vue.portapi.ws.onerror = function (error) {
   			if (vue.portapi.debug) console.log('connection error: ' + error);
+        vue.portapi.connected = false
+
+        console.log('lost connection, waiting...')
+        setTimeout(function(){
+          console.log('reconnecting...')
+          vue.portapi.connect();
+        }, 1000);
   		}
 
-  		vue.portapi.conn.onmessage = function (e) {
+  		vue.portapi.ws.onmessage = function (e) {
   			var reader = new FileReader()
   			reader.onload = function(e) {
           console.log("DEBUG: recv: " + e.target.result)
-          var splitted = e.target.result.split("|", 4)
+          var splitted = e.target.result.split("|")
+
+          // dirty hack :(
+          if (splitted.length > 4) {
+            splitted[3] = splitted.slice(3).join("|")
+          }
+
           if (splitted.length < 2) {
             console.log("received invalid message: " + e.target.result);
             return
@@ -71,11 +142,15 @@ function install(vue, options) {
                 }
                 console.log(splitted[3])
                 vue.set(opObject.records, splitted[2], parseObject(splitted[3]))
-                // opObject.records[splitted[2]] = parseObject(splitted[3])
               }
               break;
             case "done":
               //    127|done
+              opObject.loading = false
+              break;
+            case "success":
+              //    127|done
+              opObject.success = true
               opObject.loading = false
               break;
             case "error":
@@ -111,7 +186,7 @@ function install(vue, options) {
   			reader.readAsText(e.data);
   		}
     },
-    newRequest(name, msgType, msgText) {
+    newRequest(name, msgType, msgText, data) {
       if (name == undefined || name == null || name == "") {
         name = "#" + vue.portapi.requestCnt++;
       }
@@ -122,19 +197,32 @@ function install(vue, options) {
         success: false,
         warnings: [],
         error: null,
-        changeCnt: 0
+        changeCnt: 0,
       };
       vue.portapi.requests[name] = opObject
-      // vue.portapi.conn.send(name + "|" + msgType + "|" + msgText);
+
+      var requestText = name + "|" + msgType + "|" + msgText
+      if (data != undefined) {
+        requestText += "|" + JSON.stringify(data)
+      }
+
+      if (vue.portapi.connected) {
+        if (vue.portapi.debug) console.log('sending request: ' + requestText);
+        vue.portapi.ws.send(requestText);
+      } else {
+        if (vue.portapi.debug) console.log('queueing request: ' + requestText);
+        vue.portapi.requestQueue.push(requestText)
+      }
+
       console.log("added " + name);
       return opObject;
     }
   };
 
   vue.portapi.connect()
-  setTimeout(function(){
-    vue.portapi.conn.send("#0|qsub|query config");
-  }, 3000);
+  // setTimeout(function(){
+  //   vue.portapi.ws.send("#0|qsub|query config");
+  // }, 3000);
 
 }
 
