915f29081dab5160e9a178faf450d7fc567ed09e
Daniel <dhaavi@users.noreply.github.com>
2019-02-12 22:48:12 +0100

    Increase initial window size of app

--- app/main.go
+++ app/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/zserge/webview"
 
 func main() {
-	webview.Open("Portmaster", "http://127.0.0.1:18/", 1100, 700, true)
+	webview.Open("Portmaster", "http://127.0.0.1:18/", 1400, 900, true)
 }
