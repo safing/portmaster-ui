0c196720e224e798e31ceee0f23a59a224f2a559
Patrick Pacher <patrick.pacher@gmail.com>
2020-08-25 09:24:00 +0200

    Add FontAwesome 5 package

--- modules/portmaster/src/app/app.module.ts
+++ modules/portmaster/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
