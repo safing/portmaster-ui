45a056b70cad3ba298032e67505fde400aa55e10
Patrick Pacher <patrick.pacher@gmail.com>
2020-11-05 20:42:53 +0100

    Add electron based wrapper application

---
+++ app-electron/src/main.ts
@@ -0,0 +1,43 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    width: 800,
+  });
+
+  // and load the index.html of the app.
+  //mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadURL("http://localhost:817/");
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
