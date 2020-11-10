0054f7e58a4d8cd26163f9f23779e38b4b042858
Patrick Pacher <patrick.pacher@gmail.com>
2020-11-10 09:35:17 +0100

    Add version information and control buttons

--- modules/portmaster/src/app/pages/settings/settings.ts
+++ modules/portmaster/src/app/pages/settings/settings.ts
@@ -1,8 +1,10 @@
 import { ScrollDispatcher } from '@angular/cdk/overlay';
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { BehaviorSubject, combineLatest, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-import { ConfigService, ExpertiseLevelNumber, Setting, StatusService, Subsystem } from 'src/app/services';
+import { BehaviorSubject, combineLatest, Observable, Subscription } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { ConfigService, ExpertiseLevelNumber, Setting, StatusService, Subsystem, VersionStatus } from 'src/app/services';
+import { PortapiService } from 'src/app/services/portapi.service';
+import { Record } from 'src/app/services/portapi.types';
 import { fadeInAnimation } from 'src/app/shared/animations';
 import { SaveSettingEvent } from 'src/app/shared/config/generic-setting/generic-setting';
 import { FuzzySearchService } from 'src/app/shared/fuzzySearch';
@@ -25,6 +27,8 @@ export class SettingsComponent implements OnInit, OnDestroy {
   searchTerm: string = '';
 
   settings: Setting[] = [];
+
+  versions: VersionStatus | null = null;
 
   private subscription = Subscription.EMPTY;
 
@@ -59,14 +63,53 @@ export class SettingsComponent implements OnInit, OnDestroy {
 
   constructor(
     public configService: ConfigService,
+    public statusService: StatusService,
+    private portapi: PortapiService
   ) { }
 
   ngOnInit(): void {
     this.subscription = this.configService.query('')
       .subscribe(settings => this.settings = settings);
+
+    this.statusService.getVersions()
+      .subscribe(version => this.versions = version);
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  injectTrigger(module: string, kind: string): Observable<void> {
+    return this.portapi.get<Record>(`control:module/${module}/trigger/${kind}`)
+      .pipe(map(() => { }))
+  }
+
+  reloadUI(event: Event) {
+    this.injectTrigger('ui', 'reload')
+      .subscribe(() => {
+        window.location.reload();
+      })
+  }
+
+  clearDNSCache(event: Event) {
+    this.injectTrigger('resolver', 'clear name cache').subscribe();
+  }
+
+  downloadUpdates(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.injectTrigger('updates', 'trigger update').subscribe()
+  }
+
+  shutdown(event: Event) {
+    this.injectTrigger('core', 'shutdown').subscribe();
+  }
+
+  restart(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.injectTrigger('core', 'restart').subscribe();
   }
 }
