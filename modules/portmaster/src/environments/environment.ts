efcba6c0d5eb764e6fd75289bef6d6f9bd1a37cb
Luke <luke@safing.io>
2020-12-16 13:13:49 +0100

    WIP

--- modules/portmaster/src/environments/environment.ts
+++ modules/portmaster/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //portAPI: "ws://127.0.0.1:817/api/database/v1",
-  portAPI: "ws://10.100.0.230:817/api/database/v1",
+  portAPI: "ws://127.0.0.1:817/api/database/v1",
+  //portAPI: "ws://10.100.0.230:817/api/database/v1",
 
 };
 
