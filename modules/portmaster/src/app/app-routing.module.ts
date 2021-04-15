0f6b85fd15acb27f11c251241846c5be74f5837c
Patrick Pacher <patrick.pacher@gmail.com>
2021-04-15 13:30:21 +0200

    Update modules/portmaster to latest @angular/core and @angular/cli

--- modules/portmaster/src/app/app-routing.module.ts
+++ modules/portmaster/src/app/app-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
