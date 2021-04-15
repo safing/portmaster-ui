916727f544c61c238a925bc9952d8b18960a20b7
Patrick Pacher <patrick.pacher@gmail.com>
2021-04-15 15:01:19 +0200

    Update tailwindcss and postcss

--- modules/portmaster/webpack.config.js
+++ modules/portmaster/webpack.config.js
@@ -6,13 +6,15 @@ module.exports = {
         test: /\.scss$/,
         loader: 'postcss-loader',
         options: {
-          ident: 'postcss',
-          syntax: 'postcss-scss',
-          plugins: () => [
-            require('postcss-import'),
-            require('tailwindcss'),
-            require('autoprefixer'),
-          ],
+          postcssOptions: {
+            ident: 'postcss',
+            syntax: 'postcss-scss',
+            plugins: [
+              'postcss-import',
+              'tailwindcss',
+              'autoprefixer',
+            ],
+          },
         },
       },
     ],
