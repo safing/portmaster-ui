5625dc2af991c528b07c8c372b5390b026a97d5b
Patrick Pacher <patrick.pacher@gmail.com>
2021-12-29 15:51:10 +0100

    Use hostname instead of a hardcoded localhost for websocket connections

--- modules/portmaster/src/environments/environment.prod.ts
+++ modules/portmaster/src/environments/environment.prod.ts
@@ -1,8 +1,13 @@
-export const environment = {
-  production: true,
-  //portAPI: "ws://" + location.host + "/api/database/v1",
-  //httpAPI: "/api",
-  portAPI: "ws://127.0.0.1:817/api/database/v1",
-  httpAPI: "http://127.0.0.1:817/api",
-  supportHub: "https://support.safing.io"
-};
+export const environment = new class {
+  readonly supportHub = "https://support.safing.io"
+  readonly production = true;
+
+  get httpAPI() {
+    return `http://${window.location.host}/api`
+  }
+
+  get portAPI() {
+    const result = `ws://${window.location.host}/api/database/v1`;
+    return result;
+  }
+}
