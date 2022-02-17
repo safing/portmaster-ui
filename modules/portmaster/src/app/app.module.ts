0e695c617cdfc32c0341e0e0ab74d7b041a36189
Patrick Pacher <patrick.pacher@gmail.com>
2022-02-17 11:57:48 +0100

    Add tooltip component for small inline-help

--- modules/portmaster/src/app/app.module.ts
+++ modules/portmaster/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { MarkdownModule } from 'ngx-markdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,66 +21,63 @@ import { NotificationFactoryComponent } from './debug/notification-factory/notif
 import { PortapiInspectorComponent } from './debug/portapi-inspector/portapi-inspector';
 import { NavigationComponent } from './layout/navigation/navigation';
 import { SideDashComponent } from './layout/side-dash/side-dash';
+import { AppOverviewComponent, AppSettingsPageComponent } from './pages/app-settings';
 import { MonitorApplicationViewComponent, MonitorPageComponent, NetworkOverviewComponent } from './pages/monitor';
 import { SettingsComponent } from './pages/settings/settings';
+import { SpnPageComponent } from './pages/spn';
+import { SupportPageComponent } from './pages/support';
+import { SupportFormComponent } from './pages/support/form';
 import { WidgetSettingsOutletComponent } from './pages/widget-settings-outlet/widget-settings-outlet';
-import { AccordionComponent, AccordionGroupComponent, AccordionModule } from './shared/accordion';
+import { AccordionModule } from './shared/accordion';
+import { ActionIndicatorModule } from './shared/action-indicator';
+import { AppIconComponent } from './shared/app-icon';
 import { BasicSettingComponent } from './shared/config/basic-setting/basic-setting';
-import { RuleListComponent } from './shared/config/rule-list/rule-list';
-import { RuleListItemComponent } from './shared/config/rule-list/list-item';
-import { GenericSettingComponent } from './shared/config/generic-setting/generic-setting';
-import { SecuritySettingComponent } from './shared/config/security-setting/security-setting';
+import { ConfigSettingsViewComponent } from './shared/config/config-settings';
 import { FilterListComponent } from './shared/config/filter-lists';
+import { GenericSettingComponent } from './shared/config/generic-setting/generic-setting';
+import { OrderedListComponent, OrderedListItemComponent } from './shared/config/ordererd-list';
+import { RuleListItemComponent } from './shared/config/rule-list/list-item';
+import { RuleListComponent } from './shared/config/rule-list/rule-list';
+import { SecuritySettingComponent } from './shared/config/security-setting/security-setting';
+import { CanShowConnection as CanShowConnectionPipe, CanUseRulesPipe, ConnectionExpertisePipe, ConnectionLocationPipe, ConnectionsViewComponent, IsBlockedConnectionPipe, ScopeGroupContentComponent, UngroupedConnectionContentComponent, UngroupedConnectionRowComponent } from './shared/connections-view';
 import { CountIndicatorComponent } from './shared/count-indicator/count-indicator';
+import { PrettyCountPipe } from './shared/count.pipe';
 import { CountryFlagDirective } from './shared/country-flag/country-flag';
+import { ConfirmDailogComponent, DialogComponent } from './shared/dialog';
 import { DropdownComponent } from './shared/dropdown/dropdown';
 import { DropDownItemComponent, DropDownValueDirective } from './shared/dropdown/item';
+import { ExitScreenComponent } from './shared/exit-screen/exit-screen';
 import { ExpertiseDirective } from './shared/expertise/expertise-directive';
 import { ExpertiseComponent } from './shared/expertise/expertise-switch';
+import { ExternalLinkDirective } from './shared/external-link.directive';
+import { AutoFocusDirective } from './shared/focus';
 import { FuzzySearchPipe } from './shared/fuzzySearch';
+import { LoadingComponent } from './shared/loading';
+import { MenuComponent, MenuGroupComponent, MenuItemComponent, MenuTriggerComponent } from './shared/menu';
+import { MultiSwitchComponent, SwitchItemComponent } from './shared/multi-switch';
 import { NotificationComponent } from './shared/notification/notification';
+import { PaginationContentDirective, PaginationWrapperComponent } from './shared/pagination';
+import { ProfileStatisticsComponent } from './shared/profile-stats';
 import { SafePipe } from './shared/save.pipe';
+import { ScopeLabelComponent } from './shared/scope-label';
+import { TabModule } from './shared/tabs/tabs.module';
 import { PlaceholderComponent } from './shared/text-placeholder';
+import { TimeAgoPipe } from './shared/time-ago.pipe';
+import { TipUpModule } from './shared/tipup';
 import { ToggleSwitchComponent } from './shared/toggle-switch/toggle-switch';
+import { SfngTooltipModule } from './shared/tooltip';
 import { MarkdownWidgetComponent, MarkdownWidgetSettingsComponent } from './widgets/markdown-widget';
 import { NotificationWidgetComponent, NotificationWidgetSettingsComponent } from './widgets/notification-widget';
 import { PilotWidgetComponent } from './widgets/pilot-widget';
+import { PromptWidgetComponent } from './widgets/prompt-widget';
 import { StatusWidgetComponent, StatusWidgetSettingsComponent } from './widgets/status-widget';
 import { WIDGET_DEFINTIONS } from './widgets/widget.types';
-import { AppSettingsPageComponent, AppOverviewComponent } from './pages/app-settings';
-import { ProfileStatisticsComponent } from './shared/profile-stats';
-import { ConfigSettingsViewComponent } from './shared/config/config-settings';
-import { AppIconComponent } from './shared/app-icon';
-import { MenuComponent, MenuTriggerComponent, MenuItemComponent, MenuGroupComponent } from './shared/menu';
-import { OrderedListComponent, OrderedListItemComponent } from './shared/config/ordererd-list';
-import { MultiSwitchComponent, SwitchItemComponent } from './shared/multi-switch';
-import { AutoFocusDirective } from './shared/focus';
-import { PromptWidgetComponent } from './widgets/prompt-widget';
-import { LoadingComponent } from './shared/loading';
-import { ExternalLinkDirective } from './shared/external-link.directive';
-import { ScopeLabelComponent } from './shared/scope-label';
-import { TimeAgoPipe } from './shared/time-ago.pipe';
-import { HttpClientModule } from '@angular/common/http';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { ConnectionsViewComponent, ScopeGroupContentComponent, UngroupedConnectionContentComponent, UngroupedConnectionRowComponent, ConnectionExpertisePipe, ConnectionLocationPipe, CanUseRulesPipe, IsBlockedConnectionPipe, CanShowConnection as CanShowConnectionPipe } from './shared/connections-view';
-import { ExitScreenComponent } from './shared/exit-screen/exit-screen';
-import { ConfirmDailogComponent, DialogComponent } from './shared/dialog';
-import { SupportPageComponent } from './pages/support';
-import { SupportFormComponent } from './pages/support/form';
-import { TipUpAnchorDirective, TipUpComponent, TipUpTriggerDirective, TipUpIconComponent, TipUpModule } from './shared/tipup';
-import { PaginationContentDirective, PaginationWrapperComponent } from './shared/pagination';
-import { PrettyCountPipe } from './shared/count.pipe';
-import { SpnPageComponent } from './pages/spn';
-import { TabModule } from './shared/tabs/tabs.module';
-import { ActionIndicatorModule } from './shared/action-indicator';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationComponent,
     DebugComponent,
-    //AccordionComponent,
-    //AccordionGroupComponent,
     NotificationFactoryComponent,
     PortapiInspectorComponent,
     BasicSettingComponent,
@@ -143,16 +142,11 @@ import { ActionIndicatorModule } from './shared/action-indicator';
     CanUseRulesPipe,
     CanShowConnectionPipe,
     IsBlockedConnectionPipe,
-    //IndicatorComponent,
     ExitScreenComponent,
     DialogComponent,
     ConfirmDailogComponent,
     SupportPageComponent,
     SupportFormComponent,
-    //TipUpAnchorDirective,
-    //TipUpTriggerDirective,
-    //TipUpComponent,
-    //TipUpIconComponent,
     PaginationWrapperComponent,
     PaginationContentDirective,
     SpnPageComponent,
@@ -174,6 +168,7 @@ import { ActionIndicatorModule } from './shared/action-indicator';
     AccordionModule,
     TabModule,
     TipUpModule,
+    SfngTooltipModule,
     ActionIndicatorModule
   ],
   providers: [
---
+++ modules/portmaster/src/app/shared/tooltip/tooltip-component.ts
@@ -0,0 +1,103 @@
+import { animate, AnimationEvent, style, transition, trigger } from "@angular/animations";
+import { OverlayRef } from "@angular/cdk/overlay";
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, HostBinding, Inject, InjectionToken, TemplateRef } from "@angular/core";
+
+export const SFNG_TOOLTIP_CONTENT = new InjectionToken<string | TemplateRef<any>>('SFNG_TOOLTIP_CONTENT');
+export const SFNG_TOOLTIP_OVERLAY = new InjectionToken<OverlayRef>('SFNG_TOOLTIP_OVERLAY');
+
+@Component({
+  templateUrl: './tooltip-component.html',
+  styleUrls: ['./tooltip-component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger(
+      'moveInOut',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translate{{ what }}({{ value }}) scale(0.75)' }),
+            animate('.1s ease-in',
+              style({ opacity: 1, transform: 'translate{{ what }}(0%) scale(1)' }))
+          ],
+          { params: { what: 'Y', value: '-8px' } } // default parameters
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('.1s ease-out',
+              style({ opacity: 0, transform: 'translate{{ what }}({{ value }}) scale(0.75)' }))
+          ],
+          { params: { what: 'Y', value: '8px' } } // default parameters
+        )
+      ]
+    )]
+
+})
+export class SfngTooltipComponent implements AfterViewInit {
+  /**
+   * Adds snfg-tooltip-instance class to the host element.
+   * This is used as a selector in the FlexibleConnectedPosition stragegy
+   * to set a transform-origin. That origin is then used for the "arrow" anchor
+   * placement.
+   */
+  @HostBinding('class.sfng-tooltip-instance')
+  _hostClass = true;
+
+  what = 'Y';
+  value = '8px'
+  transformOrigin = '';
+
+  _appAnimate = false;
+
+  constructor(
+    @Inject(SFNG_TOOLTIP_CONTENT) public message: string,
+    @Inject(SFNG_TOOLTIP_OVERLAY) public overlayRef: OverlayRef,
+    private elementRef: ElementRef<HTMLElement>,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  dispose() {
+    this._appAnimate = false;
+    this.cdr.markForCheck();
+  }
+
+  animationDone(event: AnimationEvent) {
+    if (event.toState === 'void') {
+      this.overlayRef.dispose();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    const observer = new MutationObserver(mutations => {
+      this.transformOrigin = this.elementRef.nativeElement.style.transformOrigin;
+      if (!this.transformOrigin) {
+        return;
+      }
+
+      const [x, y] = this.transformOrigin.split(" ");
+      if (x === 'center') {
+        this.what = 'Y'
+        if (y === 'top') {
+          this.value = '-8px'
+        } else {
+          this.value = '8px'
+        }
+      } else {
+        this.what = 'X'
+        if (x === 'left') {
+          this.value = '-8px'
+        } else {
+          this.value = '8px'
+        }
+      }
+
+      this._appAnimate = true;
+      this.cdr.detectChanges();
+    });
+
+    observer.observe(this.elementRef.nativeElement, { attributes: true, attributeFilter: ['style'] })
+  }
+}
+
