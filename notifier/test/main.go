ff58a32e772a82b21ce4ee909db8cb071560a90b
Daniel <dhaavi@users.noreply.github.com>
2022-04-20 17:23:07 +0200

    Add icon test program

---
+++ notifier/test/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	// Works:
+	// "github.com/getlantern/systray"
+
+	// Does not work:
+	"fyne.io/systray"
+
+	"github.com/safing/portmaster-ui/notifier/icons"
+)
+
+func main() {
+	systray.SetIcon(icons.GreenPNG)
+	systray.Run(onReady, nil)
+}
+
+func onReady() {
+	var i int
+	for {
+		time.Sleep(250 * time.Millisecond)
+
+		systray.SetIcon(icons.ColoredIcons[i])
+		fmt.Printf("set to icon %d\n", i)
+
+		i++
+		i %= 4
+	}
+}
