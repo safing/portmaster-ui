fc49551d9739e1628a04ed992473c4252381009b
Daniel <dhaavi@users.noreply.github.com>
2023-11-28 15:11:02 +0100

    Update electron to newest version

--- app-electron/pack-container.sh
+++ app-electron/pack-container.sh
@@ -6,4 +6,10 @@ docker run --rm -ti \
   -v ${PWD}/build-cache/electron-builder:/root/.cache/electron-builder \
   electronuserland/builder:wine "$@"
 
-# run inside container: npm run pack
+# 1. run inside container:
+# npm run pack
+
+# 2. fix permissions afterwards:
+# sudo find dist -type f -exec chmod 0644 {} +
+# sudo find dist -type d -exec chmod 0755 {} +
+# sudo find dist -exec chown $(whoami) {} +
